96bdbf73d8fb9fc95bf0050a9429843a04d9a983
kabiraj404 <59503311+>
2020-05-31 14:20:09 +0545

    info

--- R/info.R
+++ R/info.R
@@ -1,10 +1,14 @@
-#' analysisJ2Kmodel Package
+#' analyzeJ2Kmodel Package
 #'
-#' This package is create exclusively to test the water balance of the J2000 hydrological model. Currently it is in the beta mode. Comments are appreciated. Note: few considerations are to be made for running the code. 1) Make sure you have installed and loaded libraries of:"data.table", "ggplot2", "dplyr", "tidyr", "knitr". 2) As an initail set up, two input variables/paths are suggested (i) Output <- "The path where timeloop is situated (ii) Input <- "The path where the input variable are stored"
+#' This package is created exclusively to test the basics of the J2000 hydrological model. Currently it is in the beta mode. Comments are appreciated. 
+Note: few considerations are to be made for running the code. 
+1) Make sure you have installed and loaded libraries of:"data.table", "ggplot2", "dplyr", "tidyr", "knitr". 
+2) As an initail set up, two input variables/paths are suggested (i) workingFolder <- "The path where the jam file is situated
 
 #' @docType package
 #'
 #' @author Kabiraj \email{kabiraj.khatiwada@gmail.com}
 #'
-#' @name WatBal
+#' @name analyzeJ2Kmodel
 NULL
+
